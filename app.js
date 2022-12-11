1f12323684096c2615049a8cde587fad908cf90a
2111cs020189 <2111cs020189@mallareddyuniversity.ac.in>
2022-12-11 13:21:56 +0530

    This is simple social media dashboard created by mruh students

---
+++ app.js
@@ -0,0 +1,42 @@
+const toggleSwitch = document.querySelector(".toggle__input");
+
+// Check localStorage for saved theme
+const currentTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  // Make sure toggle switch matches current theme
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
+}
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+
+valueDisplays.forEach((valueDisplays) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplays.getAttribute("data-val"));
+  let duration = Math.floor(interval/endValue);
+  let counter = setInterval(function(){
+    startValue += 1;
+    valueDisplays.textContent = startValue;
+    if (startValue == endValue){
+      clearInterval(counter);
+    } 
+  })
+})
